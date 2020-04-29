339c276e21c92804f5a3a5a705b290c290f883bc
rinaldyansyahpramanaipb <60166722+rinaldyansyahpramanaipb@users.noreply.github.com>
2020-04-29 11:36:16 +0700

    Update Home_Page.php

--- Home_Page.php
+++ Home_Page.php
@@ -6,9 +6,8 @@
     </head>
     <body>
         <div id="header">
-            <h1>Halo, User!</h1>
+            <h1>Halo, User!</h1> <img id="Logo" src="logoPedes.png">
         </div>
-
         <div id="sidebar">
             <a href=""><img src="home.png"; width="20px"; height="20px">Dashboard</a> <br> <!--sidebar dashboard-->
             <a href=""><img src="email.png"; width="21px"; height="24px">Surat-surat</a> <br> <!--sidebar surat-surat-->
@@ -16,17 +15,12 @@
             <a href=""><img src="exclamation-mark.png"; width="20px"; height="20px">Buat Laporan</a> <br> <!--sidebar laporan-->
             <a href=""><img src="logout.png"; width="20px"; height="20px">Logout</a> <!--sidebar logout-->
         </div>
-
     <div class="kanan">
         <p> Selamat datang di Sistem Pelayanan Desa (PeDes)</p>
         <p id="sub"> Silahkan pilih layanan berikut:</p>
-
         <a href=""><div id="progress"><img id="notip" src="notifications.png" width="100px" height="100px"> <p id="notip">Lihat perkembangan<br>pembuatan surat</p></div></a> <div><a href=""><div id="surat"><img id="surat" src="email.png" width="100px" height="100px"> <p id="surat">Membuat surat menjadi<br>lebih mudah</p></div></a></div> <div><a href=""><div id="laporan"><img id="surat" src="exclamation-mark.png" width="100px" height="100px"> <p id="laporan">Laporkan keluhan<br>di desa</p></div></a></div> <div><a href=""><div id="logout"><img id="logout" src="logout.png" width="100px" height="100px"> <p id="logout">Logout</p></div></a></div>
 
-        <p id="footer"style="font-family: Calibri; font-size: 12; margin-left:13px; margin-top:310px">©2020 Aplikasi Pelayanan Desa(PeDes) | Credit by Admin | App. Version 1.0
-    </p>
-
+   <p id="footer"style="font-family: Calibri; font-size: 12; margin-left:13px; margin-top:310px">©2020 Aplikasi Pelayanan Desa(PeDes) | Credit by Admin | App. Version 1.0 </p>
     </div>
-
     </body>
 </html>
