6a9ed40345da1733a8248fbc02a7a4b2264967e3
rinaldyansyahpramanaipb <60166722+rinaldyansyahpramanaipb@users.noreply.github.com>
2020-04-29 10:12:13 +0700

    Add files via upload

---
+++ Login_Page.php
@@ -0,0 +1,38 @@
+<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form Login PeDes</title>
+        <link rel="icon" type="image/png" href="logoPedes.png"/>
+        <link rel="stylesheet" type="text/css" href="Login_style.css">
+    </head>
+    <body>
+        <div id="main">
+            <h1>Form Login</h1>
+
+        <form>
+            <div class="inputanLogin validate-input" data-validate = "Valid NIK is required: 123388010098342">
+                        <input class="input100" type="numeric" name="NIK" placeholder="NIK">
+                    </div>
+             <div class="inputanLogin rs1 validate-input" data-validate="Password is required">
+                        <input class="input100" type="password" name="pass" placeholder="Password">
+                    </div>
+            
+                    <div class="container-login100-form-btn m-t-20">
+                        <button class="login100-form-btn">
+                            Login
+                        </button>
+                    </div>
+        </form><br>
+
+        <center> <a href="">Lupa Password</a></center><br>
+        <center><a href="">Belum punya akun?</a></center><br>
+        </div>
+    <img src="logoPedes.png">
+
+    <p style="font-family: Calibri; font-size: 12; margin-left: 550px">©2020 Aplikasi Pelayanan Desa(PeDes) | Credit by Admin | App. Version 1.0
+    </p>
+        </body>
+    </body>
+</html>
+ ?>
