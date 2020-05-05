d64b24069b6b1610b5174bd075b16bfcf8ce3cc2
rinaldyansyahpramanaipb <60166722+rinaldyansyahpramanaipb@users.noreply.github.com>
2020-05-05 22:06:13 +0700

    Add files via upload

---
+++ Pedes/SuratKTM.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Surat Keterangan Tidak Mampu</title>
+        <link rel="stylesheet" type="text/css" href="SuratKTM.css">
+        <link rel="icon" type="image/png" href="logoPedes.png"/>
+    </head>
+    <body>
+        <div id="header">
+            <h1>Halo, User!</h1> <img id="Logo" src="logoPedes.png">
+        </div>
+
+        <div id="sidebar">
+            <a href="Home_Page.php"><img id="side" src="home.png"; width="20px"; height="20px">Dashboard</a> <br> <!--sidebar dashboard-->
+            <a href="Surats.php"><img id="side" src="email.png"; width="21px"; height="24px">Surat-surat</a> <br> <!--sidebar surat-surat-->
+            <a href="progress.php"><img id="side" src="notifications.png"; width="20px"; height="20px">Progress Pembuatan Surat</a> <br> <!--sidebar progress-->
+            <a href="laporan.php"><img id="side" src="exclamation-mark.png"; width="20px"; height="20px">Buat Laporan</a> <br> <!--sidebar laporan-->
+            <a href="Login_Page.php"><img id="side" src="logout.png"; width="20px"; height="20px">Logout</a> <!--sidebar logout-->
+        </div>
+
+    <div class="kanan"><br>
+        <div id="background">
+        <p> Masukkan Data Surat Keterangan Tidak Mampu</p>
+        <br>
+        <p id="subb">Yang bertandatangan di bawah ini Kepala Desa menerangkan dengan sebenarnya bahwa:</p>
+        <form>
+            <input type="text" name="" placeholder="Nama Lengkap"><br>
+            <input type="text" name="" placeholder="Tempat, tanggal lahir"><br>
+            <input type="text" name="" placeholder="Jenis Kelamin"><br>
+            <input type="text" name="" placeholder="Alamat"><br>
+            <input type="text" name="" placeholder="Pekerjaan"><br>
+            <input type="text" name="" placeholder="Penghasilan (1 bulan)"><br>
+            <input type="text" name="" placeholder="Jumlah tanggungan"><br>
+        
+        <br>
+        <p id="subbb">Adalah orangtua dari</p>
+            <input type="text" name="" placeholder="Nama Lengkap"><br>
+            <input type="text" name="" placeholder="Tempat, tanggal lahir"><br>
+            <input type="text" name="" placeholder="Jenis Kelamin"><br>
+            <input type="text" name="" placeholder="Pekerjaan"><br>
+            <input type="text" name="" placeholder="Sekolah"><br>
+            <input type="text" name="" placeholder="Surat ini digunakan untuk..."><br>
+            <br>
+        </form>
+        <br>
+        <button onclick="" class="button">Simpan</button>
+        </div>
+    </div>
+
+    </body>
+</html>
