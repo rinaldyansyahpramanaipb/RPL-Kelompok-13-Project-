226a8e649cfee3435eb92fcc35a8ba27c6dbae8f
AlfianArief <54541136+AlfianArief@users.noreply.github.com>
2020-05-10 20:31:02 +0700

    Add files via upload

---
+++ Pedes/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo 'SELAMAT DATANG'?></title>
+        <link rel="stylesheet" type="text/css" href="style.css"> 
+    </head>
+    <body>
+        <header>
+            <div class="title">
+            <h1><?='SELAMAT DATANG'?></h1>
+        </div>
+        <div class="button">
+            <a href="Login_Page_Admin.php" class="btn">Login Admin</a>
+            <a href="Login_Page.php" class="btn">Login User</a>
+        </div>
+        </header>
+    </body>
+</html>
